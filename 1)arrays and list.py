e37d26e3b381d9b07848907d66f6c3e38b417feb
E.L.N.Prasanna <elnprasanna111@gmail.com>
2025-10-15 14:43:29 +0530

    my first dsa practice

---
+++ 1)arrays and list.py
@@ -0,0 +1,8 @@
+from array import *
+a1=array('i',[30,50,40])
+p=[]
+for c in range(len(a1)):
+    p.append(a1[c])
+q=list(p)
+q.sort()
+print(q)                                                                                                                                                                                                                                                                                                                               
